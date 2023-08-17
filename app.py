4730d13a046c01a96a2b1e9767530b0e190dc4b5
Maurício Martinello <63829313+mauriciomartinello@users.noreply.github.com>
2023-08-16 22:45:06 -0300

    Bug Added

--- app.py
+++ app.py
@@ -7,3 +7,12 @@ csrf = CSRFProtect(app)
 @app.route("/")
 def pagina_inicial():
     return "Laboratório Pipeline DevOps"
+
+@app.route('/bug')                                                                                                                                
+def bad():                                                                                                                                        
+    try:                                                                                                                                          
+        raise TypeError()                                                                                                                         
+    except TypeError as e:                                                                                                                        
+        print(e)                                                                                                                                  
+    except TypeError as e:                                                                                                                        
+        print("Duplicado, ou seja, nunca vai entrar aqui.")
